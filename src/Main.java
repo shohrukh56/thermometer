a7745ac2d708a3c46f11261444147cefc6b12a4b
shohrukh5665 <shohrukh5665@gmail.com>
2022-12-11 16:16:41 -0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,62 @@
+import java.util.*;
+public class Main {
+    static int battery=100;
+
+
+    static int getTemp() {
+        System.out.println("Please enter input within 5 seconds");
+        Scanner scan = new Scanner(System.in);
+        int input= scan.nextInt();
+        return input;
+    }
+
+
+    static void displayTemp(int input) {
+        battery-=5;
+        System.out.println("your temperature is: "+ input);
+    }
+
+    static  boolean checkTemp(int temperature) {
+        if(temperature>-40 && temperature<50 ) {
+            return true;
+        }else{
+            System.out.println("Error occurred, temperature not in range");
+        }
+        return false;
+    }
+
+    static  boolean selfCheck() {
+        if(battery>0){
+            int temp= getTemp();
+            checkTemp(temp);
+            //if temp is not within range,restart program
+            if(!checkTemp(temp)){
+                selfCheck();
+            }
+            displayTemp( temp);
+            return false;
+        }
+        else{
+            System.out.println("Battery low charge battery");
+            System.exit(0);
+        }
+        return true;
+    }
+
+    public static void main(String[] args)
+    {
+
+        final Timer timer = new Timer();
+// Note that timer has been declared final, to allow use in anon. class below
+        timer.schedule( new TimerTask()
+                        {
+                            public void run()
+                            {
+                                System.out.println("5 Seconds past please enter temperature");
+                                selfCheck();
+                            }
+                        }, 5000, 5000 //Note the second argument for repetition
+        );
+    }
+
+}
